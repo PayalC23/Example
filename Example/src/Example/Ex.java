3134840bf55e4bf87c1f69d01fea347e6f168fc9
payal <payal@LAPTOP-MLLCE3TH>
2024-05-30 22:08:40 +0530

    First Commit

---
+++ Example/src/Example/Ex.java
@@ -0,0 +1,18 @@
+package Example;
+
+public class Ex {
+
+	public static void main(String[] args) {
+	
+		int a=10,b=5;
+				
+				int c = a+b;
+				System.out.println("Addition is :"+c );
+			}
+
+		
+
+
+	}
+
+
